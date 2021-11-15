8afe0faae47388f9caa17910581da86d1a2ae857
877wrangler <50122649+877wrangler@users.noreply.github.com>
2021-11-15 04:55:55 -0500

    Add files via upload
    Attempting to extract data from JSON file

---
+++ ReadfromJSON.py
@@ -0,0 +1,26 @@
+import json
+#read file
+myjsonfile = open('data/MJ.json', 'r')
+jsondata = myjsonfile.read()
+
+#parse
+obj = json.loads(jsondata)
+
+print(str(obj['symbol']))
+
+list1 = (obj['callExpDateMap'])
+print(list1)
+print(len(list1))
+date = list1.get('2021-11-19:5')
+print(list1.get('2021-11-19:5'))
+print(date.get('15.5'))
+strike = date.get('15.5')
+print(strike)
+
+#strike = list.date.get('15.5')
+#print(strike.get('mark'))
+with open('output.txt', 'w') as f:
+    f.write(str((strike)))
+
+#for i in range(len(list)):
+#    print("Is", list[i].get('mark'))
